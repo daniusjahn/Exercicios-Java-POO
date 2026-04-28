b7c84995870894392d46053fc5f692affa98e331
daniusjahn <140536944+daniusjahn@users.noreply.github.com>
2026-04-28 11:36:19 +0000

    Criando uma verificação de idade em java.

---
+++ VERIFICARIDADE/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.err.println("Olá, seja bem-vindo ao programa de verificação de idade para carteira de habilitação!wwww");
+    }
+}
