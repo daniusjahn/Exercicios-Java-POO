0f8d6014b6242c8be90ef9b7d6c3087cb3f92962
Carlos Daniel <140536944+daniusjahn@users.noreply.github.com>
2026-05-05 08:18:11 -0300

    Change passing grade condition to include 7

--- Aula 0004/PROF_ALUNO/src/aluno.java
+++ Aula 0004/PROF_ALUNO/src/aluno.java
@@ -1,7 +1,7 @@
 public class aluno extends pessoa {
     double nota;
     void verificarNota(){
-        if(this.nota > 7){
+        if(this.nota >= 7){
             System.out.println("Aprovado");
         }
         else{
